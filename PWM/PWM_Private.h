5e5b4f0cf4bf39c9469d7075ee400d0263564633
AhmedElnaggar <es-ahmed.elnagar2027@alexu.edu.eg>
2026-04-16 07:35:46 +0200

    organization

--- PWM/PWM_Private.h
+++ PWM/PWM_Private.h
@@ -2,15 +2,20 @@
 #define PWM_PRIVATE_H
 #include "LIB/STD_TYPES.h"
 
-#define TCCR0A *((volatile u8*)0x44)
-#define TCCR0B *((volatile u8*)0x45)
-#define TCNT0 *((volatile u8*)0x46)
-#define OCR0A *((volatile u8*)0x47)
-#define OCR0B *((volatile u8*)0x48)
+// Timer/Counter1 control Registers
 #define TCCR1A *((volatile u8*)0x80)
 #define TCCR1B *((volatile u8*)0x81)
+
+// Timer/Counter1 count value store Registers
 #define TCNT1L *((volatile u8*)0x84)
 #define TCCNT1H *((volatile u8*)0x85)
-s
+
+// Timer/Counter1 TOP and compare value store Registers
+#define ICR1L *((volatile u8*)0x86)
+#define ICR1H *((volatile u8*)0x87)
+#define OCR1AL *((volatile u8*)0x88)
+#define OCR1AH *((volatile u8*)0x89)
+#define OCR1BL *((volatile u8*)0x8A)
+#define OCR1BH *((volatile u8*)0x8B)
 
 #endif // PWM_PRIVATE_H
